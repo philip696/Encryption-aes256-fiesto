ba4df8311ba3177c4668ec82f5bbdc3f0f4e60e8
philip696 <philip.dewanto@gmail.com>
2024-04-25 15:47:43 +0700

    update

--- shell_exec.php
+++ shell_exec.php
@@ -1,3 +1,9 @@
 <?php        
-shell_exec("./shrinkpdf-master/shrinkpdf.sh -r 90 -o ./shrinkpdf-master/out.pdf ./shrinkpdf-master/kenneth.pdf");
+
+$CompressedFile = //Filepath to compressed file
+$UncompressedFile = //Filepath to uncompressed file
+
+shell_exec("./shrinkpdf-master/shrinkpdf.sh -r 90 -o ./shrinkpdf-master/$CompressedFile ./shrinkpdf-master/$UncompressedFile");
+// can run using terminal $ php shell_exec.php
 ?>
+
